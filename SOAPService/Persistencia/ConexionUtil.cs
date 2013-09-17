b45db01413c09ae00f8cb0d6ab17b28c90507524
jrojasmesias <jrojasmesias@gmail.com>
2013-09-17 04:42:54 -0500

    Primer Commit con el proyecto funcionando Localmente

---
+++ SOAPService/Persistencia/ConexionUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOAPService.Persistencia
+{
+    public class ConexionUtil
+    {
+        public static string ObtenerCadena()
+        {
+            return "Data Source=a21511b6-ef0c-42e8-9db9-a23c008abf6a.sqlserver.sequelizer.com;Initial Catalog=dba21511b6ef0c42e89db9a23c008abf6a;User Id=owbgyjgtvqkikhoz;Password=GRsdrHUr2maZQr5WfN5PKeXBR2SzrQVs6AuA5EH8iUvTvxdGRJoUvZtB3YtxXt8j;";
+        }
+    }
+}
